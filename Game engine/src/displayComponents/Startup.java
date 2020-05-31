1d1273ffda0f32347c45662ec00592295ecf2c21
t4canty <t4canty@gmail.com>
2020-05-30 21:08:07 -0700

    Added comments to startup

--- Game engine/src/displayComponents/Startup.java
+++ Game engine/src/displayComponents/Startup.java
@@ -26,29 +26,39 @@ import driver.Driver;
 import fileIO.ImageLoader;
 import gameObjects.Player;
 
+/**
+ *	
+ * Created May 30, 2020
+ * @author t4canty
+ * @author TJ178
+ * Class for an animated startup screen that also spawns the ImageLoader Thread
+ * 
+ */
+
 public class Startup extends JPanel implements ActionListener{
+	//========Varibles========//
 	private boolean debug;
-	private Dimension d;
-	private String t;
+	private boolean maxed = false;
+	private boolean animationFinished = false;
 	private boolean isJar;
-	JFrame f;
-	ImageLoader i;
-	JButton start;
-	JLabel pictureLabel;
-	JPanel selectPanel;
-	JRadioButton marine;
-	JRadioButton wsb;
-	JRadioButton s;
 	private int id = Player.MARINE;
 	private int alpha = 255;
 	private float a2 = 0.0f;
-	private boolean maxed = false;
-	private boolean animationFinished = false;
-	final long StartTime = System.currentTimeMillis();
+	private Dimension d;
+	private String t;
+	private JFrame f;
+	private ImageLoader i;
+	private JButton start;
+	private JLabel pictureLabel;
+	private JPanel selectPanel;
+	private JRadioButton marine;
+	private JRadioButton wsb;
+	private JRadioButton s;
 	private Image logo;
 	
 	
 	public Startup(Dimension bounds, String title, boolean debug, boolean isJar) {
+		//====Pre-Setup====//
 		//Load images before ImageLoader
 		ImageIcon Placeholder = null;
 		try {
@@ -59,66 +69,61 @@ public class Startup extends JPanel implements ActionListener{
 			e.printStackTrace();
 		}
 		
-		i = new ImageLoader();
+		i = new ImageLoader();															//Start imageLoader  thread
 		i.start(isJar); 
+		
+		//====Setup====//
 		f = new JFrame("Startup");
 		this.debug = debug;
 		d = bounds;
 		t = title;
 		selectPanel = new JPanel();
-		try {UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());} 		 //Replace later with custom buttons - but for now better than the ugly default
-		catch (ClassNotFoundException | InstantiationException | IllegalAccessException| UnsupportedLookAndFeelException e1) {e1.printStackTrace();}
-		
-		start = new JButton("Start");
-		start.setActionCommand("l");
-		start.addActionListener(this);
-		start.setEnabled(false);
-		
-		JPanel anotherFuckingPanelJustForButtons = new JPanel();
-		anotherFuckingPanelJustForButtons.setLayout(new GridLayout(1, 3));
-		
 		marine = new JRadioButton("Marine");
 		wsb = new JRadioButton("WSB");
 		s = new JRadioButton("Secret");
+		start = new JButton("Start");
+		JPanel anotherFuckingPanelJustForButtons = new JPanel();
+		pictureLabel = new JLabel(Placeholder);
+		ButtonGroup bg = new ButtonGroup();
+		try {UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());} 		 //Replace later with custom buttons - but for now better than the ugly default
+		catch (ClassNotFoundException | InstantiationException | IllegalAccessException| UnsupportedLookAndFeelException e1) {e1.printStackTrace();}
+		
+		//====Component setup====//
+		f.setResizable(false);
+		f.setBackground(Color.white);
+		f.setSize(800, 800);
+		
+		start.setActionCommand("l");
+		start.addActionListener(this);
+		start.setEnabled(false);
 		marine.setActionCommand("m");
 		marine.addActionListener(this);
 		wsb.setActionCommand("w");
 		wsb.addActionListener(this);
 		s.setActionCommand("s");
 		s.addActionListener(this);
+		s.setEnabled(false);
+		wsb.setEnabled(false);
+		marine.setEnabled(false);
 		
-		pictureLabel = new JLabel(Placeholder);
-
-		ButtonGroup bg = new ButtonGroup();
-
 		bg.add(marine);
 		bg.add(wsb);
 		bg.add(s);
 		
-		s.setEnabled(false);
-		wsb.setEnabled(false);
-		marine.setEnabled(false);
-	
-		f.setResizable(false);
-		f.setBackground(Color.white);
-		f.setSize(800, 800);
-		this.setLayout(new BorderLayout());
-		
-		selectPanel.setLayout(new BorderLayout());
-		selectPanel.add(pictureLabel);
-		
+		anotherFuckingPanelJustForButtons.setLayout(new GridLayout(1, 3));
 		anotherFuckingPanelJustForButtons.add(marine);
 		anotherFuckingPanelJustForButtons.add(wsb);
 		anotherFuckingPanelJustForButtons.add(s);
 		
+		selectPanel.setLayout(new BorderLayout());
+		selectPanel.add(pictureLabel);
 		selectPanel.add(anotherFuckingPanelJustForButtons, BorderLayout.PAGE_END);
 		
-		
+		this.setLayout(new BorderLayout());
 		this.add(selectPanel);
-		
 		this.add(start, BorderLayout.PAGE_END);
+		
 		f.add(this);
-
 		f.setVisible(true);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Timer t = new Timer(30, this);
@@ -129,18 +134,17 @@ public class Startup extends JPanel implements ActionListener{
 	public void paint(Graphics g) {
 		super.paintComponents(g);
 		g.setColor(new Color(0, 0, 0, alpha));
-		g.fillRect(0, 0, f.getWidth(), f.getHeight());
-	
-		AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, a2);
+		g.fillRect(0, 0, f.getWidth(), f.getHeight()); 								//Animated black screen
+		
+		AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, a2);//Animated logo
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setComposite(ac);
 		g2d.drawImage(logo, f.getWidth()/2 - logo.getWidth(null)/2, f.getHeight()/2 - logo.getHeight(null)/2, null);
-		
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(!animationFinished) {
+		if(!animationFinished) {													//Function to fade out logo
 			if(a2 < 1 && !maxed)
 				a2 += 0.01f;
 			else if(a2 >= 1) {
@@ -154,14 +158,14 @@ public class Startup extends JPanel implements ActionListener{
 				a2 = 0f;
 			}
 		}
-		if(animationFinished && alpha == 0) {
+		
+		if(animationFinished && alpha == 0) {										//Re-enable buttons after the animation completes
 			s.setEnabled(true);
 			wsb.setEnabled(true);
 			marine.setEnabled(true);
 		}
 		
-		
-		if(a2 > 1) { a2 = (float) 1; }
+		if(a2 > 1) { a2 = (float) 1; }												//Error correction
 		if(alpha > 0 && animationFinished) alpha /= 1.2;
 		
 		repaint();
