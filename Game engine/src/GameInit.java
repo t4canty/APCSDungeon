7d1b78aa7412845eea74601a2a469f7b8c12b02a
t4canty <t4canty@gmail.com>
2020-06-16 15:41:17 -0700

    Added a fun little minigame for headless linux.

--- Game engine/src/GameInit.java
+++ Game engine/src/GameInit.java
@@ -1,8 +1,10 @@
 import java.awt.Dimension;
+import java.awt.GraphicsEnvironment;
 import java.awt.Toolkit;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import displayComponents.HeadlessGame;
 import displayComponents.LinuxStartup;
 import displayComponents.Startup;
 
@@ -12,7 +14,8 @@ public class GameInit{
 	public GameInit(Path filePath, String title, boolean debug, Dimension bounds, boolean isJar, String path){
 		if(debug) System.out.println("Path:" + path);
 		if(os == 0) {new Startup(bounds, title, debug, isJar, path);}
-		else {new LinuxStartup(bounds, title, debug, isJar, path);}
+		else if(os == 1) {new LinuxStartup(bounds, title, debug, isJar, path);}
+		
 		if(debug) System.out.println("Created Startup.");
 	}
 	//========Main========//
@@ -22,16 +25,22 @@ public class GameInit{
 			if(args[0].equals("true")) debug = true;
 			else debug = false;
 		}
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		String env;
-		if(System.getProperty("os.name").toLowerCase().indexOf("win") != -1) {
-			env = System.getenv("APPDATA") + "\\";
-			os = 0;
-		}else {
-			env = System.getenv("HOME") + "/";
-			os = 1;
+		if(debug) System.out.println("Headless:" + GraphicsEnvironment.isHeadless());
+		if(GraphicsEnvironment.isHeadless()) {
+			new HeadlessGame();
 		}
-		//For now, the game fills up the max area of a screen, but the scale code should theoretically allow for any square resolution.
-		GameInit gameInit = new GameInit(Paths.get(".").toAbsolutePath(), "test", debug, new Dimension(screenSize.height - 50, screenSize.height -50), true, env);
+		else {
+			Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+			String env;
+			if(System.getProperty("os.name").toLowerCase().indexOf("win") != -1) {
+				env = System.getenv("APPDATA") + "\\";
+				os = 0;
+			}else {
+				env = System.getenv("HOME") + "/";
+				os = 1;
+			}
+			//For now, the game fills up the max area of a screen, but the scale code should theoretically allow for any square resolution.
+			GameInit gameInit = new GameInit(Paths.get(".").toAbsolutePath(), "test", debug, new Dimension(screenSize.height - 50, screenSize.height -50), true, env);
+		}
 	}
 }
